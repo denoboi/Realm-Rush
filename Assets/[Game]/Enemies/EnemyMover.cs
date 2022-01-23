1e6f1ccaab58f5e9b217d596375a7da163a2b0be
denoboi <deniz.kaymakci@hotmail.com>
2022-01-23 23:53:30 +0300

    Waypoint Coroutine and Enemy Movement

--- Assets/[Game]/Enemies/EnemyMover.cs
+++ Assets/[Game]/Enemies/EnemyMover.cs
@@ -6,20 +6,25 @@ public class EnemyMover : MonoBehaviour
 {
 
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] private float waitTime = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        PrintWaypointName();
+        StartCoroutine(FollowPath());
+        
+
     }
 
    
 
-    void PrintWaypointName()
+    IEnumerator FollowPath()
     {
         foreach(Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime);
         }
     }
 }
