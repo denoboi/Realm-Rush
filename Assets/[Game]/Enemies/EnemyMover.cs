ca9fec165a77d778d122ab1405a64bf903f38083
denoboi <deniz.kaymakci@hotmail.com>
2022-01-25 01:22:40 +0300

    Smooth Enemy Movement

--- Assets/[Game]/Enemies/EnemyMover.cs
+++ Assets/[Game]/Enemies/EnemyMover.cs
@@ -6,7 +6,7 @@ public class EnemyMover : MonoBehaviour
 {
 
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] private float waitTime = 1f;
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +22,20 @@ public class EnemyMover : MonoBehaviour
     {
         foreach(Waypoint waypoint in path)
         {
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            while(travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+
             
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
         }
     }
 }
--- Assets/[Game]/Tiles/Waypoint.cs
+++ Assets/[Game]/Tiles/Waypoint.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    Waypoint waypoint;
+   
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(transform.name);
     }
 }
