9d08bfe7f7c8376691369922b1c68a9b33fe3647
denoboi <deniz.kaymakci@hotmail.com>
2022-03-06 15:30:27 +0300

    EnemyMover

--- Assets/[Game]/Enemies/EnemyMover.cs
+++ Assets/[Game]/Enemies/EnemyMover.cs
@@ -12,16 +12,36 @@ public class EnemyMover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        FindPath();
         StartCoroutine(FollowPath());
-        
+
 
     }
 
-   
+
+    /* Normalde tag ile yapmiyorduk. Path'i elle veriyorduk. Simdi tag ile yaptigimiz icin bunlari bulacagiz array'de.
+     * Daha sonra bunlari listemize ekleyecegiz FindPath methodu icinde. */
+
+    void FindPath()
+    {
+        //guard statement(yani oncekini siliyor ve yeni ekliyor)
+        path.Clear();
+
+        //simdilik bu iyi bir yontem degil, cunku hangi sirayla alacagini bilmiyoruz. Yine elle vermemiz gerek. Ileride degisecek.
+        GameObject[] waypoints = GameObject.FindGameObjectsWithTag("Path");
+
+
+        foreach (GameObject waypoint in waypoints)
+        {
+            path.Add(waypoint.GetComponent<Waypoint>());
+        }
+    }
+
 
     IEnumerator FollowPath()
     {
-        foreach(Waypoint waypoint in path)
+        foreach (Waypoint waypoint in path)
         {
             Vector3 startPosition = transform.position;
             Vector3 endPosition = waypoint.transform.position;
@@ -29,14 +49,14 @@ public class EnemyMover : MonoBehaviour
 
             transform.LookAt(endPosition);
 
-            while(travelPercent < 1f)
+            while (travelPercent < 1f)
             {
                 travelPercent += Time.deltaTime * speed;
                 transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
                 yield return new WaitForEndOfFrame();
             }
 
-            
+
         }
     }
 }
