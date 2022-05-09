91c9732bc1a4e7df4ed926f78a12d8d6d59645c4
denoboi <deniz.kaymakci@hotmail.com>
2022-05-10 02:42:00 +0300

    TargetLocator.cs
    We search and find the nearest enemy in order to aim and shoot.

--- Assets/[Game]/Tower/TargetLocator.cs
+++ Assets/[Game]/Tower/TargetLocator.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    //range gerek cok uzaga atiyorlar.
+    [SerializeField] private float _range = 15f;
+
+    //turning on/off particles
+    [SerializeField] ParticleSystem _projectileParticles;
     Transform target;
   
     void Start()
@@ -34,19 +39,44 @@ public class TargetLocator : MonoBehaviour
 
         foreach(Enemy enemy in enemies)
         {
-            //Vector3'un distance metodu tam aradigimiz iki vektor arasindaki uzakligi veriyor.
-            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            //Vector3'un distance metodu tam aradigimiz, iki vektor arasindaki uzakligi veriyor.
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position); 
+            
+            //simdi elimizde maxdistance ve target distance oldugu icin bunlari kiyaslayip karsimizdaki dusman maxdistance'tan yakinsa onu bulacagiz yani closest olani :
+            if(targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                //simdi de her defasinda maxdistance'i currentdistance'a esitleyip yeni bir enemy daha yakindaysa ona odaklatacagiz.
+                maxDistance = targetDistance;
+
+            }
         }
+
+        //target'i bulmak icin yukarida bir suru hesaplama yaptik, simdi asagida da target'i kullaniyorum en yakindakine aim almasi icin.
+        target = closestTarget;
     }
 
     //burada objeye kitleniyor ancak sadece birine kitleniyor. En yakinindaki objeye kitlenip ates etmesi icin farkli bir sey yapmaliyiz. Onu da FindClosestEnemy metodunda yapacagiz.
     private void AimWeapon()
     {
+
+        float targetDistance = Vector3.Distance(transform.position, target.position);
+
+        //ates etmesek bile surekli target'a bakacak ancak range az ise ates etmeyecek(yani particle gondermeyecek)
         weapon.LookAt(target);
+
+        if (targetDistance < _range)
+        
+            Attack(true);
+        
+        else
+            Attack(false);
     }
 
-    private void FireWeapon()
+    //Eger ates ediyorsa o zaman particle oynatacak.
+    private void Attack(bool isActive)
     {
-        
+      var emmisionModule = _projectileParticles.emission;
+          emmisionModule.enabled = isActive;
     }
 }
